39e25d41793e804dffcf0db7eafe13b797c1161b
eaalvarado8 <eric_alvarado2202@hotmail.com>
2022-07-18 16:51:43 -0500

    SUbida

---
+++ demo02/src/components/TaskItems.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="tasks_container">
+        <div class="tasks_content">
+            <h1>Listado de Estudiantes</h1>
+            <!--ul class="tasks_list"-->
+                <span v-for="numerost in tasks" :key="numerost.id">
+                    <p><b>Telefono:</b> {{ numerost.telefono }}</p>
+                    <p><b>Tipo:</b>{{ numerost.tipo }}</p>
+                    <hr>
+                </span>
+            <!--/ul-->
+        </div>
+    </div>
+  <hr>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // tasks
+                tasks: [''],
+                telefono: '',
+                tipo: '',
+            }
+        },
+        methods: {
+            async getData() {
+                try {
+                    // fetch tasks
+                    const response = await this.$http.get('http://localhost:8000/api/numerost/');
+                    // set the data returned as tasks
+                    this.tasks = response.data;
+                } catch (error) {
+                    // log the error
+                    console.log(error);
+                }
+            }
+          },
+        created() {
+            // Fetch tasks on page load
+            this.getData();
+        }
+    }
+</script>
